83e9c434251919a735c91412f1420356166d1861
TrungH012006 <trunghuynh.01.2006@gmail.com>
2025-10-28 12:26:04 -0500

    Add placeholder pop up button code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Load Navbar
+  // load header & footer
   fetch("includes/navbar.html")
     .then(res => res.ok ? res.text() : Promise.reject(res))
     .then(html => (document.getElementById("navbar").innerHTML = html))
     .catch(err => console.error("Error loading navbar:", err));
 
-  // Load Footer
   fetch("includes/footer.html")
     .then(res => res.ok ? res.text() : Promise.reject(res))
     .then(html => (document.getElementById("footer").innerHTML = html))
     .catch(err => console.error("Error loading footer:", err));
-});
+
+  // popup
+  const popup = document.getElementById("wip-popup");
+  const openBtn = document.getElementById("openPopup");
+  const closeBtn = document.querySelector(".close");
+
+  openBtn.addEventListener("click", () => {
+    popup.style.display = "flex";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+
+  });
+
